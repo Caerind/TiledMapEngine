119c04e57f67034814ecafc58437204b990fb4d9
Cmdu76 <charles.mailly@free.fr>
2014-11-12 19:43:38 +0100

    Cleaned some Tileset & Image

--- include/Image.hpp
+++ include/Image.hpp
@@ -4,7 +4,21 @@
 #include <memory>
 #include <string>
 
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/Texture.hpp>
+
+/*
+
+To Do :
+
+-Understand Data
+-Conversion sf::Color <-> std::string(Trans)
+-Get Format from Source
+-Get Width & Height from Image
+
+*/
+
 
 class Image
 {
@@ -12,10 +26,38 @@ class Image
         std::shared_ptr<Image> Ptr;
         std::shared_ptr<sf::Texture> TexturePtr;
 
+        /*
+        struct Data
+        {
+
+        };
+        */
+
     public:
         Image();
 
+        TexturePtr getTexture() const;
+        std::string getFormat() const;
+        std::string getSource() const;
+        std::string getTrans() const;
+        int getWidth() const;
+        int getHeight() const;
+
+        void setTexture(TexturePtr texture);
+        void setFormat(std::string const& format);
+        void setSource(std::string const& source);
+        void setTrans(std::string const& trans);
+        void setWidth(int width);
+        void setHeight(int height);
+
     private:
+        TexturePtr mTexture;
+
+        std::string mFormat; // Used for embedded images, in combination with a data child element. Valid values are file extensions like png, gif, jpg, bmp, etc. (since 0.9.0)
+        std::string mSource; // The reference to the tileset image file (Tiled supports most common image formats).
+        std::string mTrans; // Defines a specific color that is treated as transparent (example value: "#FF00FF" for magenta). Up until Tiled 0.10, this value is written out without a # but this is planned to change.
+        int mWidth; // The image width in pixels (optional, used for tile index correction when the image changes)
+        int mHeight; // The image height in pixels (optional)
 };
 
 #endif // TME_IMAGE_HPP
--- include/Tileset.hpp
+++ include/Tileset.hpp
@@ -3,79 +3,117 @@
 
 #include <memory>
 #include <string>
+//#include <vector>
 
 #include <SFML/Graphics/Texture.hpp>
-#include <SFML/System/Vector2.hpp>
+
+#include "Image.hpp"
+#include "Properties.hpp"
+
+
+/*
+
+To Do :
+
+-Understand TileOffset
+-Understand Terrain
+-Understand TerrainTypes
+-Understand Terrain member in Tile
+-Use & handle mSource for .tsx file
+
+*/
 
 class Map;
 
-class Tileset
+class Tileset : public Image, public Properties
 {
     public:
         typedef std::shared_ptr<Tileset> Ptr;
 
+        /*
         struct TileOffset
         {
+            TileOffset();
 
+            int x; // Horizontal offset in pixels
+            int y; // Vertical offset in pixels (positive is down)
+        };
+        */
 
+        /*
+        class Terrain : public Properties
+        {
+            public:
+                Terrain();
+
+                std::string getName() const;
+                int getTile() const;
+
+                void setName(std::string const& name);
+                void setTile(int tile);
+
+            private:
+                std::string mName; // The name of the terrain type.
+                int mTile; // The local tile-id of the tile that represents the terrain visually.
         };
 
-        struct Tile
+        typedef std::vector<Terrain> TerrainTypes;
+        */
+
+        class Tile : /*public Image,*/ public Properties
         {
-            Tile();
+            public:
+                Tile();
 
+                int getId() const;
+                //std::string getTerrain() const;
+                float getProbability() const;
 
+                void setId(int id);
+                //void setTerrain(std::string const& terrain);
+                void setProbability(float probability);
 
-            Properties mProperties;
-        };
-
-        struct TerrainTypes
-        {
-
-        };
-
-        struct Terrain
-        {
-
+            private:
+                int mId; // The local tile ID within its tileset.
+                //std::string mTerrain; // Defines the terrain type of each corner of the tile, given as comma-separated indexes in the terrain types array in the order top-left, top-right, bottom-left, bottom-right. Leaving out a value means that corner has no terrain. (optional) (since 0.9.0)
+                float mProbability; // A percentage indicating the probability that this tile is chosen when it competes with others while editing with the terrain tool. (optional) (since 0.9.0)
         };
 
     public:
         Tileset(Map* map);
 
-        // Getters
         int getFirstGid() const;
+        //std::string getSource() const;
         std::string getName() const;
-        sf::Vector2i getTileSize() const;
+        int getTileWidth() const;
+        int getTileHeight() const;
         int getSpacing() const;
         int getMargin() const;
-        std::string getSource() const;
-        std::shared_ptr<sf::Texture> getTexture() const;
-        sf::Vector2i getTextureSize() const;
+        //TileOffset& getTileOffset();
+        //TerrainTypes& getTerrainsTypes() const;
 
-        // Setters
         void setFirstGid(int first);
+        //void setSource(std::string const& source);
         void setName(std::string const& name);
-        void setTileSize(sf::Vector2i tileSize);
+        void setWidth(int width);
+        void setHeight(int height);
         void setSpacing(int spacing);
         void setMargin(int margin);
-        void setSource(std::string const& source);
-        void setTexture(std::shared_ptr<sf::Texture> texture);
-        void setTextureSize(sf::Vector2i textureSize);
+        //void setTileOffset(TileOffset offset);
+        //void setTerrainType(TerrainTypes ttypes);
 
     private:
         Map* mMap;
 
-        // Tileset Part
-        int mFirstGid;
-        std::string mName;
-        sf::Vector2i mTileSize;
-        int mSpacing;
-        int mMargin;
-
-        // Image Part
-        std::string mSource;
-        std::shared_ptr<sf::Texture> mTexture;
-        sf::Vector2i mTextureSize;
+        int mFirstGid; // The first global tile ID of this tileset (this global ID maps to the first tile in this tileset).
+        //std::string mSource; // If this tileset is stored in an external TSX (Tile Set XML) file, this attribute refers to that file. That TSX file has the same structure as the <tileset> element described here. (There is the firstgid attribute missing and this source attribute is also not there. These two attributes are kept in the TMX map, since they are map specific.)
+        std::string mName; // The name of this tileset.
+        int mWidth; // The (maximum) width of the tiles in this tileset.
+        int mHeight; // The (maximum) height of the tiles in this tileset.
+        int mSpacing; // The spacing in pixels between the tiles in this tileset (applies to the tileset image).
+        int mMargin; // The margin around the tiles in this tileset (applies to the tileset image).
+        //TileOffset mTileOffset;
+        //TerrainTypes mTerrainTypes;
 };
 
 #endif // TME_TILESET_HPP
