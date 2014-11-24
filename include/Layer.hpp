7a0e9335f460d6e0dfdcdc757f07529bd5f6b4fd
Cmdu76 <charles.mailly@free.fr>
2014-11-24 23:35:33 +0100

    Layer

--- include/Layer.hpp
+++ include/Layer.hpp
@@ -6,12 +6,15 @@
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/Graphics/Vertex.hpp>
-#include <SFML/System/Time.hpp>
 
 #include "ILayer.hpp"
 #include "Tileset.hpp"
+#include "Tile.hpp"
 
 class Map;
+
+namespace tme
+{
 
 class Layer : public ILayer
 {
@@ -19,64 +22,22 @@ class Layer : public ILayer
         typedef std::shared_ptr<Layer> Ptr;
         typedef std::pair<int,int> Pos;
 
-        class Tile : public sf::Drawable, public sf::Transformable
-        {
-            public:
-                Tile();
-
-                void load(Map* map, int id);
-                void setTilePos(int x, int y, int width, int height);
-
-                int getId() const;
-                void setId(int id);
-
-                Tileset::Ptr getTileset() const;
-                void setTileset(Tileset::Ptr tileset);
-
-                void setOpacity(float opacity);
-
-            private:
-                virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
-            private:
-                int mId; // The global tile ID.
-
-                sf::Vertex mVertices[4];
-                Tileset::Ptr mTileset;
-        };
-
-        typedef std::map<Pos,Tile> TileMap;
-
     public:
         Layer(Map* map);
 
         void render(sf::RenderTarget& target, sf::RenderStates states);
 
-        int getWidth() const;
-        int getHeight() const;
-
-        void setWidth(int width);
-        void setHeight(int height);
-
-        Tile getTile(int x, int y);
         int getTileId(int x, int y);
-        TileMap getTiles() const;
-        std::map<Pos,int> getTilesIds();
-
-        void setTile(int x, int y, Tile tile);
         void setTileId(int x, int y, int id);
-        void setTiles(TileMap const& tiles);
-        void setTilesIds(std::map<Pos,int> const& tiles);
 
         void setOpacity(float opacity);
 
     private:
         Map* mMap;
 
-        int mWidth; // The width of the layer in tiles. Traditionally required, but as of Tiled Qt always the same as the map width.
-        int mHeight; // The height of the layer in tiles. Traditionally required, but as of Tiled Qt always the same as the map height.
-
-        TileMap mTiles;
+        std::map<Pos,Tile> mTiles;
 };
+
+} // namespace tme
 
 #endif // TME_LAYER_HPP
--- src/Layer.cpp
+++ src/Layer.cpp
@@ -1,91 +1,9 @@
 #include "../include/Layer.hpp"
 #include "../include/Map.hpp"
 
-////////////////////////////////////////////////////////////
-// LAYER::TILE                                            //
-////////////////////////////////////////////////////////////
-Layer::Tile::Tile() : mId(0), mTileset(nullptr)
+namespace tme
 {
-}
 
-////////////////////////////////////////////////////////////
-void Layer::Tile::load(Map* map, int id)
-{
-    setId(id);
-    mTileset = map->getTileset(mId);
-    if (mTileset != nullptr)
-    {
-        setPosition(mTileset->getTileOffset().x, mTileset->getTileOffset().y);
-
-        sf::IntRect tRect = mTileset->getTextureRect(mId);
-
-        mVertices[0].texCoords = sf::Vector2f(tRect.left, tRect.top);
-        mVertices[1].texCoords = sf::Vector2f(tRect.left + mTileset->getTileWidth(), tRect.top);
-        mVertices[2].texCoords = sf::Vector2f(tRect.left + mTileset->getTileWidth(), tRect.top + mTileset->getTileHeight());
-        mVertices[3].texCoords = sf::Vector2f(tRect.left, tRect.top + mTileset->getTileHeight());
-    }
-}
-
-////////////////////////////////////////////////////////////
-void Layer::Tile::setTilePos(int x, int y, int width, int height)
-{
-    mVertices[0].position = sf::Vector2f(x, y);
-    mVertices[1].position = sf::Vector2f(x + width, y);
-    mVertices[2].position = sf::Vector2f(x + width, y + height);
-    mVertices[3].position = sf::Vector2f(x, y + height);
-}
-
-////////////////////////////////////////////////////////////
-int Layer::Tile::getId() const
-{
-    return mId;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::Tile::setId(int id)
-{
-    mId = id;
-}
-
-////////////////////////////////////////////////////////////
-Tileset::Ptr Layer::Tile::getTileset() const
-{
-    return mTileset;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::Tile::setTileset(Tileset::Ptr tileset)
-{
-    mTileset = tileset;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::Tile::setOpacity(float opacity)
-{
-    sf::Color a = sf::Color::White;
-    a.a = 255.f * opacity;
-    for (int i = 0; i < 4; i++)
-    {
-        mVertices[i].color = a;
-    }
-}
-
-////////////////////////////////////////////////////////////
-void Layer::Tile::draw(sf::RenderTarget& target, sf::RenderStates states) const
-{
-    if (mTileset != nullptr)
-    {
-        states.transform *= getTransform();
-        if (mTileset->getTexture() != nullptr)
-        {
-            states.texture = mTileset->getTexture().get();
-        }
-        target.draw(mVertices, 4, sf::Quads, states);
-    }
-}
-
-////////////////////////////////////////////////////////////
-// LAYER                                           //
 ////////////////////////////////////////////////////////////
 Layer::Layer(Map* map) : mMap(map)
 {
@@ -98,109 +16,16 @@ void Layer::render(sf::RenderTarget& target, sf::RenderStates states)
     if (mVisible)
     {
         states.transform *= getTransform();
-        for (int j = 0; j < mHeight; j++)
-        {
-            for (int i = 0; i < mWidth; i++)
-            {
+        for (int j = 0; j < mSize.y; j++)
+            for (int i = 0; i < mSize.x; i++)
                 target.draw(mTiles[Layer::Pos(i,j)]);
-            }
-        }
     }
-}
-
-////////////////////////////////////////////////////////////
-int Layer::getWidth() const
-{
-    return mWidth;
-}
-
-////////////////////////////////////////////////////////////
-int Layer::getHeight() const
-{
-    return mHeight;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::setWidth(int width)
-{
-    mWidth = width;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::setHeight(int height)
-{
-    mHeight = height;
-}
-
-////////////////////////////////////////////////////////////
-Layer::Tile Layer::getTile(int x, int y)
-{
-    return (x >= 0 && x < mWidth && y >= 0 && y < mHeight) ? mTiles[Layer::Pos(x,y)] : Tile();
 }
 
 ////////////////////////////////////////////////////////////
 int Layer::getTileId(int x, int y)
 {
-    return (x >= 0 && x < mWidth && y >= 0 && y < mHeight) ? mTiles[Layer::Pos(x,y)].getId() : 0;
-}
-
-////////////////////////////////////////////////////////////
-Layer::TileMap Layer::getTiles() const
-{
-    return mTiles;
-}
-
-////////////////////////////////////////////////////////////
-std::map<Layer::Pos,int> Layer::getTilesIds()
-{
-    std::map<Layer::Pos,int> map;
-    for (int i = 0; i < mWidth; i++)
-    {
-        for (int j = 0; j < mHeight; j++)
-        {
-            map[Layer::Pos(i,j)] = mTiles[Layer::Pos(i,j)].getId();
-        }
-    }
-    return map;
-}
-
-////////////////////////////////////////////////////////////
-void Layer::setTile(int x, int y, Tile tile)
-{
-    if (x >= 0 && x < mWidth && y >= 0 && y < mHeight)
-    {
-        if(tile.getTileset() != nullptr && mMap != nullptr)
-        {
-            int mapTileWidth = mMap->getTileWidth();
-            int mapTileHeight = mMap->getTileHeight();
-            int width = tile.getTileset()->getTileWidth();
-            int height = tile.getTileset()->getTileHeight();
-
-            if (mMap->getOrientation() == "orthogonal")
-            {
-                tile.setTilePos(x * mapTileWidth, y * mapTileHeight, width, height);
-            }
-            else if (mMap->getOrientation() == "isometric")
-            {
-                tile.setTilePos((x-y) * mapTileWidth * 0.5,(x+y) * mapTileHeight * 0.5,width,height);
-            }
-            else if (mMap->getOrientation() == "staggered")
-            {
-                int posX;
-                if ((y % 2) == 0)
-                {
-                    posX = x * mapTileWidth;
-                }
-                else
-                {
-                    posX = x * mapTileWidth + mapTileWidth /2;
-                }
-                tile.setTilePos(posX,y * mapTileHeight / 2,width,height);
-            }
-
-            mTiles[Layer::Pos(x,y)] = tile;
-        }
-    }
+    return (x >= 0 && x < mSize.x && y >= 0 && y < mSize.y) ? mTiles[Layer::Pos(x,y)].getId() : 0;
 }
 
 ////////////////////////////////////////////////////////////
@@ -210,21 +35,32 @@ void Layer::setTileId(int x, int y, int id)
     if (mMap != nullptr)
     {
         tile.load(mMap,id);
-        setTile(x,y,tile);
+        if (x >= 0 && x < mSize.x && y >= 0 && y < mSize.y)
+        {
+            if(tile.getTileset() != nullptr && mMap != nullptr)
+            {
+                int mapTileWidth = mMap->getTileWidth();
+                int mapTileHeight = mMap->getTileHeight();
+                int width = tile.getTileset()->getTileSize().x;
+                int height = tile.getTileset()->getTileSize().y;
+
+                if (mMap->getOrientation() == "orthogonal")
+                    tile.setTilePos(x * mapTileWidth, y * mapTileHeight, width, height);
+                else if (mMap->getOrientation() == "isometric")
+                    tile.setTilePos((x-y) * mapTileWidth * 0.5,(x+y) * mapTileHeight * 0.5,width,height);
+                else if (mMap->getOrientation() == "staggered")
+                {
+                    int posX;
+                    if ((y % 2) == 0)
+                        posX = x * mapTileWidth;
+                    else
+                        posX = x * mapTileWidth + mapTileWidth / 2;
+                    tile.setTilePos(posX,y * mapTileHeight / 2,width,height);
+                }
+                mTiles[Layer::Pos(x,y)] = tile;
+            }
+        }
     }
-}
-
-////////////////////////////////////////////////////////////
-void Layer::setTiles(Layer::TileMap const& tiles)
-{
-    mTiles = tiles;
-    // Update tiles
-}
-
-////////////////////////////////////////////////////////////
-void Layer::setTilesIds(std::map<Layer::Pos,int> const& tiles)
-{
-    // Update tiles
 }
 
 ////////////////////////////////////////////////////////////
@@ -232,7 +68,7 @@ void Layer::setOpacity(float opacity)
 {
     mOpacity = opacity;
     for (auto itr = mTiles.begin(); itr != mTiles.end(); itr++)
-    {
         itr->second.setOpacity(opacity);
-    }
 }
+
+} // namespace tme
